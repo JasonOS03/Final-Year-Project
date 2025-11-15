0068452db3529d3f3b904329f8d3e81d2e610e2b
JasonOS03 <=>
2025-11-15 15:02:39 +0000

    modified code to allow the server response to be displayed on the webpage. The API used is OpenRouter API. The switch of API use is due to Hugging Face API not being compatible. It attempted to use many different Hugging Face models but to no avail. Upon discovering the OpenRouter API, the API was integrated into the system using the Express.js fetch command. The response generated by the API is sent to the frontend, and a HTML container element was created to hold this response and display it.

--- public/expressserver.js
+++ public/expressserver.js
@@ -15,23 +15,43 @@ app.use(express.static("public"));
             try {
         
 
-        const resp = await fetch("https://router.huggingface.co/hf-inference", {
+        const resp = await fetch("https://openrouter.ai/api/v1/chat/completions", {
             method: "POST",
             headers: {
-            "Authorization": "Bearer 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",
-            "Content-Type": "application/json"
+            "Authorization": "Bearer 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",
+            "Content-Type": "application/json",
+            "HTTP-Referer": "http://localhost:3000", // or your deployed domain
+            "X-Title": "SaaS Idea Generator"
             
             },
-            body: JSON.stringify({ inputs: user_prompt, model: "gpt2" })
+            body: JSON.stringify({  model: "openrouter/auto", messages: [
+    { role: "system", content: "none" },
+    { role: "user", content: user_prompt }
+  ], max_tokens: 50 })
 
         });
 
+            if (!resp.ok) 
+    {
+        const errorText = await resp.text();
+        console.error("Model API error:", errorText);
+        return response.status(resp.status).json({ error: errorText });
+    }
+
         const result = await resp.json();
+        const content = result.choices[0].message.content
         const formatted = JSON.stringify(result);
         const insertion_response = await the_database.insert({ user_prompt,the_output: formatted });
         console.log("Inserted document:", { user_prompt, the_output: formatted });
-        response.json({db: insertion_response, output: formatted});
         console.log("Model output:", formatted);
+        if(!content)
+        {
+            console.log("content is empty");
+        }
+        else
+        {
+            response.json({db: insertion_response, output: content});
+        }
         } catch (err) {
             console.error("Error inserting prompt to database",err);
         }
