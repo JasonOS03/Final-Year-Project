c73f6dae7fc298fe893b014ae509bd2e5e65c42a
JasonOS03 <=>
2025-11-25 19:25:35 +0000

    Fixed the functionality to retrieve the latest recommendation and display it on the webpage when the webpage is initially loaded. The latest recommendation displays in the container and these recommendations will be tied to specific users and user sessions in further development. New error checking functionalities were added to ensure that if there is no recommendation to be retreived, a response is returned to the frontend indicating that no recommmendations have been found.

--- public/homepage.js
+++ public/homepage.js
@@ -13,8 +13,9 @@ try{
     }
     )
     const retrieved_data = await retrieval.json();
-    const retrieved_output = retrieved_data.output;
-    response_text.innerHTML = retrieved_output.trim();
+    const retrieved_output = JSON.parse(retrieved_data.output);
+    const response_content = retrieved_output.choices[0].message.content;
+    response_text.innerHTML = response_content.trim();
 }catch(err)
 {
     console.log("failed to retrieve recommendation");
@@ -22,23 +23,34 @@ try{
 
 });
 
+// listens for user form submission
 user_form.addEventListener("submit", async (e)=>{
-    e.preventDefault();
-    const user_prompt =  input.value.trim();
+    e.preventDefault(); // prevents default reloading of page
+    const user_prompt =  input.value.trim();// trims whitespace characters from the user prompt
+    if(user_prompt.length <= 20) 
+        {
+            alert("prompt is too short")
+            return;
+        }
 
 try{
+    // send prompt to Express.js server using the HTTP POST method.
 const response = await fetch("/submit-prompt", 
 {
     method: "POST",
     headers: {
             "Content-Type": "application/json"
             },
+            // convert the user prompt to string format 
+            // and this prompt is contained within the body
             body: JSON.stringify({user_prompt}) 
 
 })
+    // wait for the JSON response from the server
     const server_response = await response.json();
-    const trimmed_response = server_response.output;
-    response_text.innerHTML = trimmed_response.trim();
+    const trimmed_response = server_response.output; //  extract output field from response
+    response_text.innerHTML = 
+    trimmed_response.trim(); // replace the current HTML text with the response
     console.log(server_response);
 }catch(err)
 {
