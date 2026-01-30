e4491094d4e06adf7bbb9f28b58b879cce9955b0
JasonOS03 <=>
2026-01-30 10:50:08 +0000

    added the functionality for handling user sessions. When the user clicks the submit button, the JS frontend sends a POST request containing their username and password. In the express backend, this request is recieved and the backend attempts to find exactly one username and password matching the requested username and password. If found the user's username is stored within their session and a response message is returned indicating success. The login functionality is present in login.js for the frontend logic and expressserver.js for the backend logic

--- public/login.js
+++ public/login.js
@@ -1,19 +1,49 @@
-const username = document.getElementById("username");
-const password =  document.getElementById("password");
+const username = document.getElementById("username").value;
+const password =  document.getElementById("password").value;
 const login_submit =  document.getElementById("login_submit");
+const login_form = document.getElementById("login_form");
+const error_message = document.getElementById("credential_message");
 
-login_submit.addEventListener("submit" ,async ()=> {
+login_submit.addEventListener("click" ,async (e)=> {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
     try{
-         const login_details = await fetch("/user_details",{
-            method: "GET",
+         const login_details = await fetch("/user_login",{
+            method: "POST",
             headers:
             {
                 "Content-Type": "application/json"
-            }
+            },
+            body: JSON.stringify
+            ({
+                username:username,
+                password:password
+            })
+            
 
          })
+         const resp = await login_details.json();
+         
+         if(resp.success)
+        {
+         login_form.innerHTML = "Correct credentials entered, redirecting to homepage";
+        setTimeout(()=>{
+           window.location.href = "index.html" 
+        },2000);
+        }
+        else
+        {
+            error_message.innerHTML = "password or username does not match, try again";
+        }
+
     }
     catch{
         console.log("failed to fetch user details");
     }
+
+
+
 })
+
+
