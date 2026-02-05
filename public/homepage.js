9d55fc09f8c79f285ed467d63fd5fa856ef15209
JasonOS03 <=>
2026-02-05 12:55:54 +0000

    Added the logic to expand the container once the view full recommendation button is clicked. When clicked the offsetWidth and offsetHeight are multiplied by 1.8 to increase the size. The view full recommendations button is removed here, using the removeChild function. A new button displays in the top right corner of the container allowing the user to return to the container back to normal size so they can view other recommendations. When the view full recomms button is clicked the container has a boxShadow and outline to make it appear as if the container has been highlighted. This logic was added in the homepage.js file

--- public/homepage.js
+++ public/homepage.js
@@ -24,7 +24,34 @@ try{
     }
     const message = res?.choices?.[0]?.message; 
     const response_content = message.reasoning_details?.[0]?.summary?.trim() || message.reasoning?.trim() || message.content?.trim();
-    response_text.innerHTML = response_content.trim().replace(/"/g, ""); // remove double quotes from the response
+    response_text.innerHTML = response_content.trim().replace(/"/g, "");
+
+    view_full_button.addEventListener("click",()=>{
+        const container = document.getElementById("container");
+        const container_height = container.offsetHeight;
+        const container_width = container.offsetWidth;
+        container.style.width = container_width *1.8 + "px";
+        container.style.height = container_height *1.8 + "px";
+        container.style.position = "relative";
+        container.style.outline = "4px solid black";
+        container.style.boxShadow = "0 0 10px black";
+        container.removeChild(view_full_button);
+        const x_button = document.createElement("button")
+        x_button.textContent = "X";
+        container.appendChild(x_button);
+        x_button.style.top = 0;
+        x_button.style.right = 0;
+        x_button.style.position = "absolute";
+        x_button.addEventListener("click",()=>{
+            container.style.width = container_width + "px";
+            container.style.height = container_height + "px";
+            container.style.boxShadow = "";
+            container.style.outline  = "";
+
+            x_button.remove();
+        })
+
+    }) // remove double quotes from the response
 }catch(err)
 {
     console.log("failed to retrieve recommendation");
