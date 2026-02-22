91bbc613ab7eee121a5625e6d9f25b5f5dd3cf25
JasonOS03 <=>
2026-02-22 12:49:45 +0000

    Logic has been added to ensure that any updates to the users profile (product portfolio and idea list) results in a new recommendation being generated on the homepage. instead of just comparing the length of the new ideas vs old ideas and length of old prouducts vs new products, The JS code simply compares the stringified versions of these directly. JSON.Stringify was required here as the backend returns JSON documents from the CouchDB database. Once stringified these are compared, and if there are any differences between the old and newa new recommendation is generated on the homepage. The next step is to refine this logic to ensure that when a new recommendation is generated it will be entirely unique to previous recommendations

--- public/login.js
+++ public/login.js
@@ -29,6 +29,23 @@ login_submit.addEventListener("click" ,async (e)=> {
          if(resp.success)
         {
          login_form.innerHTML = "Correct credentials entered, redirecting to homepage";
+
+         login_form.appendChild(document.createElement("br"));
+
+         const spinner = document.createElement("div")
+         spinner.classList.add("spinner-border text-warning");
+         spinner.setAttribute("role","status");
+
+         const span =  document.createElement("span");
+         span.textContent = "Redirecting....";
+         span.classList.add("visually-hidden");
+
+         spinner.style.height = "5rem";
+         spinner.style.width = "5rem";
+
+         spinner.appendChild(span);
+         login_form.appendChild(spinner);
+
         setTimeout(()=>{
            window.location.href = "homepage.html" 
         },2000);
